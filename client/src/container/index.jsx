ccd4a4e1b0bd7f49a4ba75672466263192f01498
RyomenDev <aakashvatsh007@gmail.com>
2025-04-21 16:44:14 +0530

    deploy: setup

--- client/src/container/index.jsx
+++ client/src/container/index.jsx
@@ -1,7 +1,7 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 
-import Contact from "./Contact.jsx";
+import Contact from "./Contact";
 import GoogleMap from "./Map.jsx";
 
 export { Header, Contact, GoogleMap, Footer };
